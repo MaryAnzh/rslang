c8cab8baf5a654552e9d18ed1ccf16071d4070f2
MaryAnzh <MaryAnzh@mail.ru>
2022-02-03 19:19:22 +0300

    fix: style

--- rs-lang/src/views/components/header/header.tsx
+++ rs-lang/src/views/components/header/header.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './header.scss';
-import logo from '../../../img/logo.svg';
 import { Logo } from '../Logo/Logo';
 //import { render } from '@testing-library/react';
 
@@ -17,7 +16,8 @@ class Header extends React.Component {
         </ul>
         <ul className="header__sign">
           <li>Вход</li>
-          <li className="header__sign-icon"><img src="https://raw.githubusercontent.com/MaryAnzh/rslang-assets/32072b0672f4d7289dc4b4af3117022d1cfe5ce7/assets/svg/sign-in.svg"></img></li>
+          <li className="header__sign-icon">
+            <img src="https://raw.githubusercontent.com/MaryAnzh/rslang-assets/32072b0672f4d7289dc4b4af3117022d1cfe5ce7/assets/svg/sign-in.svg" alt='sign in'></img></li>
         </ul>
       </header>
     );
