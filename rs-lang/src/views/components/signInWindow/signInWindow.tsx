76a4586a24a8e7b5f6d4d97b7e141b0ac8d96d60
MaryAnzh <MaryAnzh@mail.ru>
2022-02-03 19:25:39 +0300

    fix: lint fix

--- rs-lang/src/views/components/signInWindow/signInWindow.tsx
+++ rs-lang/src/views/components/signInWindow/signInWindow.tsx
@@ -1,5 +1,13 @@
-class signInWindow {
+import React from 'react';
+import './header.scss';
+//import { render } from '@testing-library/react';
 
+class SignInWindow extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
 }
 
-export { signInWindow };
+export { SignInWindow };
