d478e759c6698acabd88d19e3bf3e95d56be94b0
MaryAnzh <MaryAnzh@mail.ru>
2022-02-04 17:15:31 +0300

    feat: add style for register form

--- rs-lang/src/views/components/signInWindow/signInWindow.tsx
+++ rs-lang/src/views/components/signInWindow/signInWindow.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './signInWindow.scss';
 import { PopUp } from './../../elements/popUp/popUp';
-//import { render } from '@testing-library/react';
+import { RegisterForm } from '../RegisterForm/registerForm';
 
 class SignInWindow extends React.Component {
   render() {
     return (
       <PopUp>
-        <h3 className='pop-up__body__sign-in-title'>Войти</h3>
+        <RegisterForm />
       </PopUp>
     );
   }
