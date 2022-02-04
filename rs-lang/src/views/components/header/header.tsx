34f901406aa755259ac8afe80d2d6bd802cec223
MaryAnzh <MaryAnzh@mail.ru>
2022-02-04 18:53:25 +0300

    fix: style

--- rs-lang/src/views/components/header/header.tsx
+++ rs-lang/src/views/components/header/header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './header.scss';
 import { Logo } from '../Logo/Logo';
 import { Link } from 'react-router-dom';
+import { SignInWindow } from '../signInWindow/signInWindow';
 //import { render } from '@testing-library/react';
 
 class Header extends React.Component {
@@ -25,7 +26,7 @@ class Header extends React.Component {
           </li>
         </ul>
         <ul className="header__sign">
-          <li>Вход</li>
+          <li onClick={(e) => this.signInOnClick(e)} >Вход</li>
           <li className="header__sign-icon"><img src="https://raw.githubusercontent.com/MaryAnzh/rslang-assets/32072b0672f4d7289dc4b4af3117022d1cfe5ce7/assets/svg/sign-in.svg"></img></li>
         </ul>
       </header>
