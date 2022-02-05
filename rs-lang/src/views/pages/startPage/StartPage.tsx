9269d1711cb12c8269c9ad8b0392c91a06f10325
Sergey Masiuk <mayerror@gmail.com>
2022-02-05 04:35:58 +0300

    feat: add home page layout

--- rs-lang/src/views/pages/startPage/StartPage.tsx
+++ rs-lang/src/views/pages/startPage/StartPage.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import './startPage.scss';
 import logo from '../../../img/logo.svg';
+import { BenefitCard } from '../../components/BenefitCard/BenefitCard';
+import { PersonCard } from '../../components/PersonCard/PersonCard';
 //import { render } from '@testing-library/react';
 
 class StartPage extends React.Component {
   render() {
     return (
       <main className="start-page main">
-        <img src={logo} className="start-page-logo" alt="logo" />
+        <h2 className="start-page__title">Оцените возможности и преимущества приложения</h2>
+        <div className="start-page__list">
+          <BenefitCard>dfsdfsdfsdfs</BenefitCard>
+          <BenefitCard />
+          <BenefitCard />
+          <BenefitCard />
+        </div>
+        <h2 className="start-page__title">Наша команда</h2>
+        <div className="start-page__list">
+          <PersonCard />
+          <PersonCard />
+        </div>
       </main>
     );
   }
