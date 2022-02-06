3ad24147dac3c8bac5bc795027057c535cdece51
Sergey Masiuk <mayerror@gmail.com>
2022-02-06 15:23:58 +0300

    feat: implement benefits card

--- rs-lang/src/views/components/BenefitCard/BenefitCard.tsx
+++ rs-lang/src/views/components/BenefitCard/BenefitCard.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './BenefitCard.scss';
 
-class BenefitCard extends React.Component {
+type BenefitType = {
+  url: string;
+  title: string;
+  text: string;
+}
+
+class BenefitCard extends React.Component<BenefitType> {
   render() {
     return (
       <div className="benefit-card">
-        карточка
+        <img className="benefit-card__icon" src={this.props.url} alt="icon" />
+        <div className="benefit-card__wrapper">
+          <h3 className='benefit-card__title'>{this.props.title}</h3>
+          <p>{this.props.text}</p>
+        </div>
       </div>
     );
-  }    
+  }
 }
 
 export { BenefitCard };
