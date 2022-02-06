d794c9816b45f3fe638124d1c9adae9d5fea0d11
MaryAnzh <MaryAnzh@mail.ru>
2022-02-06 19:24:46 +0300

    feat: add book sections

--- rs-lang/src/views/pages/TextBook/TextBook.tsx
+++ rs-lang/src/views/pages/TextBook/TextBook.tsx
@@ -5,11 +5,20 @@ class TextBook extends React.Component {
   render() {
     return (
       <main className="main">
-        <div className='book-wrap'>
+        <div className='book-page-wrap'>
           <h1>У ч е б н и к</h1>
-          <div className='book-wrap__book'>
-            <div className='book-wrap__book__page-wrap'>
-
+          <div className='book-page-wrap__book-wrap'>
+            <div className='book-page-wrap__book-wrap__book'>
+              <div className='book-page-wrap__book-wrap__book__sections'>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+                <div className='book-page-wrap__book-wrap__book__sections__section'></div>
+              </div>
+              
             </div>
           </div>
         </div>        
