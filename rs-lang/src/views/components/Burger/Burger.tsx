9baf21399d9ea7d006ba23b7e20500df58d48a39
MaryAnzh <MaryAnzh@mail.ru>
2022-02-11 19:15:19 +0300

    feat: header style

---
+++ rs-lang/src/views/components/Burger/Burger.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Burger.scss';
+
+type BurgerType = {
+
+}
+
+class Burger extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export { Burger };
