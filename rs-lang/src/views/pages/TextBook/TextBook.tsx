81f58cf5cf43364fd5108dfe5b38f41a0ee6775b
Sergey Masiuk <mayerror@gmail.com>
2022-02-11 17:33:29 +0300

    feat: implement group pagination

--- rs-lang/src/views/pages/TextBook/TextBook.tsx
+++ rs-lang/src/views/pages/TextBook/TextBook.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { newDataService } from '../../../dataServer/dataService';
 import { TextBookState, WordCardProps, WordCardType } from '../../../interfaces/types';
+import { GroupPagination } from '../../components/GroupPagination/GroupPagination';
 import { Pagination } from '../../components/Pagination/Pagination';
 import { WordCard } from '../../components/WordCard/WordCard';
 import './TextBook.scss';
@@ -23,6 +24,7 @@ class TextBook extends React.Component {
     };
     this.PageDownHandler = this.PageDownHandler.bind(this);
     this.PageUpHandler = this.PageUpHandler.bind(this);
+    this.GroupHandler = this.GroupHandler.bind(this);
   }
 
 
@@ -31,6 +33,23 @@ class TextBook extends React.Component {
       return true;
     } else {
       return false;
+    }
+  }
+
+  GroupHandler(group: number) {
+    if (this.state.group !== group) {
+      newDataService.getWords(group, this.state.page).then(response => {
+        this.setState((prev: TextBookState) => {
+          if (prev.words !== response) {
+            return {
+              words: response,
+            };
+          }
+        });
+      });
+      this.setState(() => ({
+        group: group,
+      }));
     }
   }
 
@@ -77,7 +96,7 @@ class TextBook extends React.Component {
   }
 
   render() {
-    console.log(this.state.page);
+    console.log(`Page: ${this.state.page}; Group: ${this.state.group}`);
     let words: JSX.Element[] | '' = [];
     if (this.state.words.length) {
       words = this.state.words.map((word, index) => <WordCard key={index} word={word}/>)
@@ -91,6 +110,7 @@ class TextBook extends React.Component {
           <div className='book-page-wrap__book-wrap'>
             <div className='book-page-wrap__controls'>
               <Pagination downHandler={this.PageDownHandler} upHandler={this.PageUpHandler} page={this.state.page + 1}/>
+              <GroupPagination groupHandler={this.GroupHandler}/>
             </div>
             <div className='book-page-wrap__book-wrap__book'>
               <div className='book-page-wrap__card-container'>
