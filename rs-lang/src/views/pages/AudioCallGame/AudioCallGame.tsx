142beb4a23ef7457d54d1440d30be18dab29716f
MaryAnzh <MaryAnzh@mail.ru>
2022-02-14 21:42:10 +0300

    feat: add game elements

--- rs-lang/src/views/pages/AudioCallGame/AudioCallGame.tsx
+++ rs-lang/src/views/pages/AudioCallGame/AudioCallGame.tsx
@@ -3,19 +3,38 @@ import './AudioCallGame.scss';
 import '../../elements/settingPanel/settingPanel';
 import { audioCallPageModel } from '../../../model/AudioCallPageModel';
 import { SettingPanel } from '../../elements/settingPanel/settingPanel';
+import { CrossUpDate } from '../../elements/crossUpDate/crossUpDate';
+import { HeardsError } from '../../audioCallElements/heardsErrior/heardsError';
+
+type AudioCallGameType = {
+  heardFill: string,
+  heardStroke: string,
+}
 
 class AudioCallGame extends React.Component {
+  state: AudioCallGameType;
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      heardFill: '#000000',
+      heardStroke: '#000000',
+    }
+  }
+
   render() {
     if (audioCallPageModel.isSetting) {
       return (
         <main className="main">
           <div className='games-page-wrap'>
             <h1>А У Д И О  В Ы З О В</h1>
-            <div className='games-page-wrap__games-wrap'>
+            <div className='games-page-wrap__settings-wrap'>
               <SettingPanel />
-              <div className='games-page-wrap__games-wrap__audio-call'>
+              <div className='games-page-wrap__settings-wrap__audio-call'>
                 <h2>Аудиовызов</h2>
-                <div className='games-page-wrap__games-wrap__audio-call__game-start'>Играть</div>
+                <div
+                  className='games-page-wrap__settings-wrap__audio-call__game-start'
+                  onClick={(e) => { this.startGameOnClick(e) }} >Играть</div>
               </div>
             </div>
           </div>
@@ -26,13 +45,38 @@ class AudioCallGame extends React.Component {
         <main className="main">
           <div className='games-page-wrap'>
             <h1>А У Д И О  В Ы З О В</h1>
-            <div className='games-page-wrap__games-wrap'>
-              Game
+            <div className='games-page-wrap__game-wrap'>
+              <div
+                className='games-page-wrap__game-wrap__cross'
+                onClick={(e) => { this.closeGameOnClick(e) }}>
+                <CrossUpDate crossId='audio-game-cross' />
+              </div>
+              <div className='games-page-wrap__game-wrap__audio-call'>
+                <div className='games-page-wrap__game-wrap__audio-call__heards-error'>
+                  <HeardsError heardFill={this.state.heardFill} heardStroke={this.state.heardStroke} />
+                  <HeardsError heardFill={this.state.heardFill} heardStroke={this.state.heardStroke} />
+                  <HeardsError heardFill={this.state.heardFill} heardStroke={this.state.heardStroke} />
+                  <HeardsError heardFill={this.state.heardFill} heardStroke={this.state.heardStroke} />
+                  <HeardsError heardFill={this.state.heardFill} heardStroke={this.state.heardStroke} />
+                  
+                </div>
+
+              </div>
             </div>
           </div>
         </main >
       );
     }
+  }
+
+  startGameOnClick(e: React.MouseEvent<HTMLElement>) {
+    audioCallPageModel.isSetting = false;
+    this.forceUpdate();
+  }
+
+  closeGameOnClick(e: React.MouseEvent<HTMLElement>) {
+    audioCallPageModel.isSetting = true;
+    this.forceUpdate();
   }
 }
 
