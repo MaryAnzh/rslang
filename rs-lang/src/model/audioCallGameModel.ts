192367cb32c2c86b44bbe9ecc5b679787e4826cd
MaryAnzh <MaryAnzh@mail.ru>
2022-02-16 13:08:21 +0300

    fix: random number runction

--- rs-lang/src/model/audioCallGameModel.ts
+++ rs-lang/src/model/audioCallGameModel.ts
@@ -2,7 +2,7 @@ import { IAudioCallWords, IAnxwer } from '../interfaces/wordsInterface';
 import { wordsArray } from './gamaTesting';
 
 class AudioCallGameModel {
-  currenwordsArray: IAudioCallWords[];
+  currentWordsArray: IAudioCallWords[];
 
   isSetting: boolean;
 
@@ -14,10 +14,10 @@ class AudioCallGameModel {
   //массив слоа раунда
   roundWordsArray: IAudioCallWords[];
 
-  constructor(currenwordsArray: IAudioCallWords[]) {
-    this.currenwordsArray = currenwordsArray;
-    this.isSetting = true;
-    this.currentWordsArrayLangth = this.currenwordsArray.length;
+  constructor(currentWordsArray: IAudioCallWords[]) {
+    this.currentWordsArray = currentWordsArray;
+    this.isSetting = false;
+    this.currentWordsArrayLangth = this.currentWordsArray.length;
     this.currentRound = 1;
     this.roundWordsArray = [];
   }
@@ -25,34 +25,50 @@ class AudioCallGameModel {
   roundWords() {
     const roundArray: IAnxwer[] = [];
     const trueWord: IAnxwer = {
-      word: this.currenwordsArray[this.currentRound - 1].word,
+      word: this.currentWordsArray[this.currentRound - 1].word,
       trueAnxwer: true,
     };
     const falseWordsNumber = 3;
-    for (let index = 0; index < falseWordsNumber; index += 1) {
-      let randomIndex = this.rendomNumber(this.currentWordsArrayLangth);
-      let falseWord = this.currenwordsArray[randomIndex].word;
-      let check = this.currenwordsArray.findIndex((elem, i) => {
-        if (elem.word == falseWord) {
-          return i;
-        }
-        return -1;
-      });
+    for (let i = 0; i < falseWordsNumber; i++) {
+      //let randomIndex = this.rendomNumber(this.currentWordsArrayLangth);
       
+      console.log(i);
+    //   console.log('randomIndex');
+    //   console.log(randomIndex);
     }
+    
+    return '1111';
+    
+    // for (let index = 0; index < falseWordsNumber; index += 1) {
+    //   let randomIndex = this.rendomNumber(this.currentWordsArrayLangth);
+    //   console.log('randomIndex');
+    //   console.log(randomIndex);
+    //   let falseWord = this.currentWordsArray[randomIndex].word;
+    //   console.log('falseWord');
+    //   console.log(falseWord);
+    //   let check = this.currentWordsArray.findIndex((elem, i) => {
+    //     if (elem.word == falseWord) {
+    //       return i;
+    //     }
+    //     return -1;
+    //   });
+    //   console.log('check');
+    //   console.log(check);
+    // }
   }
 
   shuffle(array: Array<number>) {
-    array.sort(() => Math.random() - 0.5);
+    return array.sort(() => Math.random() - 0.5);
   }
 
   rendomNumber(digitRange: number) {
     const randomArr = [];
-    for (let i = 0; i < digitRange; i = + 1) {
+    //массив начинаетмся с 0
+    for (let i = 0; i < digitRange; i += 1) {
       randomArr.push(i);
     }
-
     this.shuffle(randomArr);
+
     return randomArr[1];
   }
 
@@ -60,5 +76,5 @@ class AudioCallGameModel {
 const a = wordsArray;
 const test = new AudioCallGameModel(a);
 console.log('Тестирование');
-console.log(test.currentWordsArrayLangth);
+console.log(test.rendomNumber(5));
 export { AudioCallGameModel };
